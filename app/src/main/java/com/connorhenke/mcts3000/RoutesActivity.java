3109ce3234ac01c494d98e99e8f465f80c9e5337
Tony DiPerna <dipernaa@msoe.edu>
2015-03-03 08:38:03 -0600

    added pull to refresh to routes activity. added refresh button to map activity.

--- app/src/main/java/com/connorhenke/mcts3000/RoutesActivity.java
+++ app/src/main/java/com/connorhenke/mcts3000/RoutesActivity.java
@@ -7,6 +7,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,66 +22,77 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RoutesActivity extends Activity {
-	
-	protected List<Route> routes = new ArrayList<Route>();
-	protected RoutesAdapter adapter;
-	ListView listView;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.routes);
-		
-    	listView = (ListView) this.findViewById(android.R.id.list);
-    	listView.setOnItemClickListener(new OnItemClickListener() {
+    protected List<Route> routes = new ArrayList<Route>();
+    protected RoutesAdapter adapter;
+    ListView listView;
+    SwipeRefreshLayout swipeLayout;
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				TextView number = (TextView) view.findViewById(R.id.number);
-				TextView name = (TextView) view.findViewById(R.id.name);
-				Intent intent = new Intent(getApplicationContext(), MapActivity.class);
-				intent.putExtra("NUMBER", number.getText());
-				intent.putExtra("NAME", name.getText());
-	        	startActivity(intent);
-				overridePendingTransition(R.anim.lefttoright, R.anim.righttoleft);
-				
-			}
-    	});
-		
-    	if(adapter == null) {
-    		adapter = new RoutesAdapter(this, R.layout.route_item, routes);
-    	}
-    	listView.setAdapter(adapter);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.routes);
 
-		new RoutesRequester().execute();
-	}
-	
-	class RoutesRequester extends AsyncTask<String, String, JSONObject> {
+        listView = (ListView) this.findViewById(android.R.id.list);
+        listView.setOnItemClickListener(new OnItemClickListener() {
 
-		@Override
-		protected JSONObject doInBackground(String... params) {
-			JSONObject routes = Constants.getRoutes();
-			Log.i(WIFI_SERVICE, "Request completed");
-			return routes;
-		}
-		
-		@Override
-	    protected void onPostExecute(JSONObject result) {
-			try {
-				JSONArray routeList = result.getJSONObject("bustime-response").getJSONArray("routes");
-				routes.clear();
-				for(int i = 0; i < routeList.length(); i++) {
-					JSONObject route = (JSONObject)routeList.get(i);
-					String rt = route.getString("rt");
-					String rtnm = route.getString("rtnm");
-					String rtclr = route.getString("rtclr");
-					routes.add(new Route(rt, rtnm, rtclr));
-				}
-				adapter.notifyDataSetChanged();
-			}catch(Exception e) {
-				
-			}
-		}
-		
-	}
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView number = (TextView) view.findViewById(R.id.number);
+                TextView name = (TextView) view.findViewById(R.id.name);
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                intent.putExtra("NUMBER", number.getText());
+                intent.putExtra("NAME", name.getText());
+                startActivity(intent);
+                overridePendingTransition(R.anim.lefttoright, R.anim.righttoleft);
+
+            }
+        });
+
+        swipeLayout = (SwipeRefreshLayout) this.findViewById(R.id.swipe_container);
+        swipeLayout.setColorSchemeResources(android.R.color.holo_blue_bright);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new RoutesRequester().execute();
+            }
+        });
+
+        if(adapter == null) {
+            adapter = new RoutesAdapter(this, R.layout.route_item, routes);
+        }
+        listView.setAdapter(adapter);
+
+        new RoutesRequester().execute();
+    }
+
+    class RoutesRequester extends AsyncTask<String, String, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            JSONObject routes = Constants.getRoutes();
+            Log.i(WIFI_SERVICE, "Request completed");
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject result) {
+            try {
+                JSONArray routeList = result.getJSONObject("bustime-response").getJSONArray("routes");
+                routes.clear();
+                for(int i = 0; i < routeList.length(); i++) {
+                    JSONObject route = (JSONObject)routeList.get(i);
+                    String rt = route.getString("rt");
+                    String rtnm = route.getString("rtnm");
+                    String rtclr = route.getString("rtclr");
+                    routes.add(new Route(rt, rtnm, rtclr));
+                }
+                swipeLayout.setRefreshing(false);
+                adapter.notifyDataSetChanged();
+            }catch(Exception e) {
+
+            }
+        }
+
+    }
 }
